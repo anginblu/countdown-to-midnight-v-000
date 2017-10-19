8d53cee2fc4ff1b4e9c7d69590bccbfa4c763986
Jacqueline Lee <anginblu@gmail.com>
2017-10-19 22:09:05 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(n)
-  while n >= 0
-    print "#{n} SECOND(S)!"
-    n -= 1
+def countdown(number)
+  while number >= 0
+    print "#{number} SECOND(S)!"
+    number -= 1
   end
   "HAPPY NEW YEAR!"
 end
